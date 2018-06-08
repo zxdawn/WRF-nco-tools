35700b7594ca6e7da773899ff4fe11a3511e1ceb
zxdawn <xinzhang1215@gmail.com>
2018-06-08 16:46:15 +0800

    Check support of Gulf of Mexico

---
+++ append_wrf_output.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script will append nolnox wrfout* to lnox wrfout*
+# and get LNO and LNO2
+
+# Set dir of wrfout* which have been extracted
+savedir='/nuist/u/home/yinyan/xin/work/BEHR/data/wrf_profiles/history/'
+lnox_dir=${savedir}lnox/
+nolnox_dir=${savedir}nolnox/
+
+# Check same files in lnox_dir and nolnox_dir
+# Then append variables and calculate
+
+files="$(comm -12 <(ls -F $lnox_dir) <(ls -F $nolnox_dir))"
+echo -e "Append these files:\n $files"
+for file in $files
+do
+    ncks -A -h $nolnox_dir$file $lnox_dir$file
+    ncap2 -v -h -O -s 'lno2=no2_lnox-no2_nolnox;lno=no_lnox-no_nolnox' $lnox_dir$file $lnox_dir$file.tmpnc
+    ncks -A -h $lnox_dir$file.tmpnc $lnox_dir$file
+    ncatted -O -h -a description,lno2,o,c,'LNO2 mixing ratio' $lnox_dir$file
+    ncatted -O -h -a description,lno,o,c,'LNO mixing ratio' $lnox_dir$file
+done
+
+rm $lnox_dir*.tmpnc
